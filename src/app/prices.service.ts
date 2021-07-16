17b2c1bad076975401bbfbe3fcc087b57d94566b
Mwila <mpnyirongo@gmail.com>
2021-07-16 21:35:30 +0200

    Removed the data.json from the prices services

--- src/app/prices.service.ts
+++ src/app/prices.service.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@angular/core'
-import { Observable, of } from 'rxjs'
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-
-const data: any = require('./components/market/prices/prices-table/data.json')
+import { Observable } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root',
 })
 export class PricesService {
-  data = data
   private dataUrl = 'http://esapp-test.herokuapp.com/prices' // URL to web api
 
   constructor(private http: HttpClient) {}
 
+  // Get the json data and return
   getData(): Observable<any[]> {
-    console.log('Fetching Data')
     const data = this.http.get<any[]>(this.dataUrl)
-    console.log(data)
     return data
   }
 }
