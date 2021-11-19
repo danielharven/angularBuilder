251a5f71e096167979da0b3791b7680cdb99d6cb
unknown <admin@SH-HE-01.GOV.ZM>
2021-11-19 09:08:29 +0200

    Added the employees interface to models

---
+++ src/app/models/employee.interface.ts
@@ -0,0 +1,14 @@
+export interface Employee {
+  _id: string
+  empName: string
+  empNo: string
+  email: string
+  nrc: string
+  published_at: string
+  createdAt: string
+  updatedAt: string
+  __v: string
+  created_by: string
+  updated_by: string
+  id: string
+}
--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,20 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { UtilitiesService } from '../../services/utilities.service'
-
-interface Employee {
-  _id: string
-  empName: string
-  empNo: string
-  email: string
-  nrc: string
-  published_at: string
-  createdAt: string
-  updatedAt: string
-  __v: string
-  created_by: string
-  updated_by: string
-  id: string
-}
+import { Employee } from '../../models/employee.interface'
 
 @Component({
   selector: 'app-employee',
